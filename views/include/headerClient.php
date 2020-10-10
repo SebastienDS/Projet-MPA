29c457043a7dc999a6517156ba737d9c5bce1733
SebastienDS <sebastien.dossantos.77@gmail.com>
2020-10-10 14:12:07 +0200

    page d'accueil client

--- views/include/headerClient.php
+++ views/include/headerClient.php
@@ -3,11 +3,26 @@
         <img src="<?= SCRIPT_NAME ?>/public/img/logo.png" width="200" height="200">
     </a>
 
-    <div>
-        
+    <div class="max-height grow">
+        <div class="headerBtn center">
+            Mes Comptes
+        </div>
+        <div class="headerBtn center">
+            Mes Impayés
+        </div>
     </div>
 
-    <a href='#'>
-        <img src="<?= SCRIPT_NAME ?>/public/img/logo.png" width="200" height="200">
-    </a>
+    <div>
+        <ul id="menu-deroulant">
+            <li>
+                <a href='#'>
+                    <img src="<?= SCRIPT_NAME ?>/public/img/logo.png" width="200" height="200">
+                </a>
+                <ul>
+                    <li><a href="#">Changer de Mot de Passe</a></li>
+                    <li><a href="#">Se déconnecter</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
 </div>
