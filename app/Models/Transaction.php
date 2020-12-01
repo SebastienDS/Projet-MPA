14feb6ecbeb7d6a41685688fe4b9ee23603951be
SebastienDS <sebastien.dossantos.77@gmail.com>
2020-12-01 21:52:08 +0100

    Update Transaction.php

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -39,14 +39,18 @@ class Transaction extends Model {
         $table = self::getTable();
         $entrepriseTable = Entreprise::getTable();
 
-        $where['id'] = $idCompte;
-        $queryCondition = implode(' and ', array_map(function($condition) {
-            return "$condition = :$condition";
+        $queryCondition = implode(' and ', array_map(function($condition) use ($where) {
+            return "`$condition` = :$condition";
         }, array_keys($where)));
 
-        $stmt = DBConnection::getPDO()->prepare("SELECT NSIREN as 'siren', Raison_Sociale as 'raisonSociale', 
-            datetr as 'date', count(datetr) as 'nombreTransactions', 'Euro', moyenPay, sum(Mtrans) as 'montantTotal' 
-            FROM {$table} JOIN {$entrepriseTable} on NSIREN = N_SIREN WHERE {$queryCondition} GROUP BY datetr, NSIREN ORDER BY {$orderBy} {$orderdirection}");
+        if ($queryCondition) {
+            $queryCondition = "HAVING {$queryCondition}";
+        }
+        $where['id'] = $idCompte;
+
+        $stmt = DBConnection::getPDO()->prepare("SELECT NSIREN as siren, Raison_Sociale as raisonSociale, 
+            datetr as date, count(datetr) as nombreTransactions, 'Euro', moyenPay, sum(Mtrans) as montantTotal 
+            FROM {$table} JOIN {$entrepriseTable} on NSIREN = N_SIREN WHERE id = :id GROUP BY datetr, NSIREN {$queryCondition} ORDER BY {$orderBy} {$orderdirection}");
         $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
         $stmt->execute($where);
         return $stmt->fetchAll();
